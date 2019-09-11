46e6d72fbcf77c63f6cb7a42cf274545e9797769
Eszter <ev821@nyu.edu>
2019-09-11 14:31:54 +0800

    Wow! We did this in class! Cool! Amazing... I can like do things kinda!!

---
+++ my-work/week-2/lab-2/d3intro/script.js
@@ -0,0 +1,29 @@
+
+let viz = d3.select("#viz-container")
+                  .append("svg")
+                    .attr("id","viz")
+                    // .attr("class", "something")
+                    .attr("width","800")
+                    .attr("height","800")
+;
+//
+// // current selction <svg> </svg>
+//
+// viz.attr("height", 500);
+//
+// let myCircle = viz.append("rect")
+//             .attr("x", 200)
+//             .attr("y", 100)
+//             .attr("width", 50)
+//             .attr("height", 50)
+// ;
+//
+// myCircle.attr("fill", "lavender");
+
+let myData = [2,57,96,34,19];
+
+viz.selectAll("circle").data(myData).enter().append("circle")
+                                          .attr("cx", 120)
+                                          .attr("cy", 400)
+                                          .attr("r", 20)
+;
