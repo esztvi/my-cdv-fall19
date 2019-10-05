bb32112ae22dba7199272c3bbacbc6efad5b4834
Eszter <ev821@nyu.edu>
2019-10-05 22:57:58 +0800

    more notes

--- my-work/data-zine/cover.js
+++ my-work/data-zine/cover.js
@@ -2,4 +2,6 @@
 /// SO I WANT TO DO THIS CIRCLE BAR GRAPH
 //That would be super cool for the cover I think...
 //Ideally I want four (like name/freguency, region/frequency, medium/frequency, day/frequency)
+//do I need for div's then? Not sure... maybe I do? I'm currently just playing around with the data set
+
 //https://www.d3-graph-gallery.com/circular_barplot
