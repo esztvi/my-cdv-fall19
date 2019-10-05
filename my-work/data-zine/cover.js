35df0fba569879dbbb7d26792c024cb431c3568b
Eszter <ev821@nyu.edu>
2019-10-05 22:54:33 +0800

    Changes and ideas to the middlespread

--- my-work/data-zine/cover.js
+++ my-work/data-zine/cover.js
@@ -1,1 +1,5 @@
 
+/// SO I WANT TO DO THIS CIRCLE BAR GRAPH
+//That would be super cool for the cover I think...
+//Ideally I want four (like name/freguency, region/frequency, medium/frequency, day/frequency)
+//https://www.d3-graph-gallery.com/circular_barplot
