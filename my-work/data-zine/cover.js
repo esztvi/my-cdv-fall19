a62c21b1f8e3f918f5c09a89a326c6356703ef61
Eszter <ev821@nyu.edu>
2019-10-08 12:17:45 +0800

    Cover complete (no legend/or key)

--- my-work/data-zine/cover.js
+++ my-work/data-zine/cover.js
@@ -5,7 +5,7 @@ let outerRadius = Math.min(w, h) / 2;   // the outerRadius goes from the middle 
 let paddingSide = 200;
 let paddingOther = 100;
 let padding = 8;
-let bandwidth = 20;
+// let bandwidth = 20;
 /// SO I WANT TO DO THIS CIRCLE BAR GRAPH
 //That would be super cool for the cover I think...
 //Ideally I want four (like name/freguency, region/frequency, medium/frequency, day/frequency)
@@ -25,97 +25,78 @@ let viz = d3.select("#container")
     .attr("width", w)
     .attr("height", h)
     .style("background-color", "lavender")
-    ;
-  // .append("g")
-  //   .attr("transform", "translate(" + width / 2 + "," + ( height/2+100 )+ ")"); // Add 100 on Y translation, cause upper bars are longer
-  // let datagroups = viz.selectAll(".datagroup").data(incomingData).enter()
-  //   .append("g")
-  //     .classed("datagroup", true)
-  // ;
-  // X scale
-  let xScale = d3.scaleBand().domain([0,71]).range([0, 2 * Math.PI]);
-  function xPosition(d, i){
-    return xScale(d.Name);
-  }
+;
 
-  function yPosition(d, i){
-    return h/2 ; //- d.height
-  }
+// .range([innerRadius, outerRadius])   // Domain will be define later.
+// .domain([0, 26]); // Domain of Y is from 0 to the max seen in the data
+let ColorScale = d3.scaleLinear().domain([0,10,26]).range(["lightblue","blue","midnightblue"]);
+function getColor(d,i){
+  return ColorScale(d.Value);
+}
 
-  function getGroupTranslation(d, i){
-    return "translate(" + w / 2 + "," + ( h/2+100 )+ ")"; // Add 100 on Y translation, cause upper bars are longer
+function getGroupTranslation(d, i){
+  return "translate(" + w / 2 + "," + ( h/2 - 10)+ ")"; // Add 100 on Y translation, cause upper bars are longer
 
-  }
-      // .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
-      // .align(0)                  // This does nothing ?
-      // .domain( data.map(function(d) { return d.Name; }) ); // The domain of the X axis is the list of states.
+}
 
+  // Add bars
+function gotData(incomingData){
+  console.log(incomingData);
+
+
+  let xScale = d3.scaleBand().domain(incomingData.map(function(d) {return d.Name})).range([0, 2 * Math.PI]);
   // Y scale
   let yScale = d3.scaleRadial().domain([0, 26]).range([innerRadius, outerRadius]);
-  function getHeight(d,i){
-    return yScale(d.Value) ;
 
-  }
-  function getPush(d,i){
-    return - yScale(d.Value) ;
+  let circleChartGroup = viz.append("g")
+    .classed("circleChartGroup",true)
+    .attr("transform",getGroupTranslation)
+  ;
 
-  }
-      // .range([innerRadius, outerRadius])   // Domain will be define later.
-      // .domain([0, 26]); // Domain of Y is from 0 to the max seen in the data
-      let ColorScale = d3.scaleLinear().domain([0,10,26]).range(["lightblue","blue","midnightblue"]);
-      function getColor(d,i){
-      return ColorScale(d.Value);
-      }
-      function getHeight(d,i){
-        return yScale(d.Value) ;
+  let datagroups = circleChartGroup.selectAll(".datagroup").data(incomingData).enter()
+    .append("g")
+      .classed("datagroup", true)
+  ;
 
-      }
-  // Add bars
-  function gotData(incomingData){
-    console.log(incomingData);
+function getName (d) {
+  return (d.Name)
+}
 
-    let datagroups = viz.selectAll(".datagroup").data(incomingData).enter()
-      .append("g")
-        .classed("datagroup", true)
-    ;
+  let arcFunction = d3.arc()     // imagine your doing a part of a donut plot
+    .innerRadius(innerRadius)
+    .outerRadius(function(d){
+      return yScale(d.Value)
+    })
+    .startAngle(function(d){
+      return xScale(d.Name)
+    })
+    .endAngle(function(d){
+      return xScale(d.Name) + xScale.bandwidth();
+    })
+    .padAngle(0.01)
+  ;
 
-    let towers = datagroups.append("rect")
-    .attr("height",getHeight)
+  let towers = datagroups
+    .append("path")
+    .classed("arcSlice", true)
+    // .attr("height",getHeight)
     .attr("fill", getColor)
-    .attr("d", d3.arc()     // imagine your doing a part of a donut plot
-        .innerRadius(innerRadius)
-        .outerRadius(function(d) { return (d.Value); })
-        .startAngle(function(d) { return (d.Name); })
-        .endAngle(function(d) { return (d.Name) + bandwidth; })
-        .padAngle(0.01)
-        .padRadius(innerRadius))
-        ;
-    //     .attr("x", 0)
-    //     .attr("y", getPush)
-    //     .attr("width", 20)
-    //     .attr("height",getHeight)
-    //     .attr("fill", getColor)
-    // ;
+    .attr("d", arcFunction)
+  ;
 
-    //
-    // let labels = datagroups.append("text")
-    //     .text(getName)
-    //     .attr("fill", "red")
-    //     .attr("transform", "rotate(90)")
-    // ;
-    //
-    // datagroups.attr("transform", getGroupTranslation);
+  let labels = datagroups
+  .append("g")
+  .attr("text-anchor", function(d) { return (xScale(d.Name) + xScale.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+  .attr("transform", function(d) { return "rotate(" + ((xScale(d.Name) + xScale.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (yScale(d['Value'])+10) + ",0)"; })
+  .append("text")
+  .text(getName)
+  .attr("transform", function(d) { return (xScale(d.Name) + xScale.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+        .style("font-size", "11px")
+      .attr("alignment-baseline", "middle")
+  // .attr("x",xScale)
 
-      datagroups.attr("transform", getGroupTranslation);
-  }
 
-      // .attr("fill", "#69b3a2")
-      // .attr("d", d3.arc()     // imagine your doing a part of a donut plot
-      //     .innerRadius(innerRadius)
-      //     .outerRadius(function(d) { return y(d.Value); })
-      //     .startAngle(function(d) { return x(d.Name); })
-      //     .endAngle(function(d) { return x(d.Name) + x.bandwidth(); })
-      //     .padAngle(0.01)
-      //     .padRadius(innerRadius));
+  // datagroups.attr("transform", getGroupTranslation);
+}
 
-          d3.json("data.json").then(gotData);
+d3.json("data.json").then(gotData);
