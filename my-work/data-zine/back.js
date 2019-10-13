7465eb14beb5e8fcf5f564b71e6e2edd3c913d6d
Eszter <ev821@nyu.edu>
2019-10-13 13:32:23 +0800

    back cover labeled

--- my-work/data-zine/back.js
+++ my-work/data-zine/back.js
@@ -1,17 +1,18 @@
 let w = 1200; //single page
 let h = 800;
-let projection = d3.geoMercator().translate([w/2, h/2]).scale(200).center([0,40]);
+let projection = d3.geoMercator().translate([w/2 +50, h/2 -100]).scale(200).center([0,40]);
 let path = d3.geoPath().projection(projection);
 
 let viz = d3.select("#container")
   .append("svg")
     .attr("width", w)
     .attr("height", h)
-    .style("background-color", "WhiteSmoke")
+    .style("background-color", "White")
     .attr("preserveAspectRatio", "xMinYMin meet")
     .attr("viewBox", "0 0 " + w + " " + h)
     .classed("svg-content", true);
 ;
+
 
 let ColorScale = d3.scaleLinear().domain([0,10,30,160,195]).range(["lightgrey","darkgrey","grey","dimgrey","black"]);
 // function getColor(d,i){
