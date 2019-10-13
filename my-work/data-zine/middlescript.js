f74f37346b1d9198a27f4013e01c35f481c41816
Eszter <ev821@nyu.edu>
2019-10-13 17:57:28 +0800

    changing the background color

--- my-work/data-zine/middlescript.js
+++ my-work/data-zine/middlescript.js
@@ -5,7 +5,7 @@ let viz = d3.select("#container")
   .append("svg")
     .attr("width", w)
     .attr("height", h)
-    .style("background-color", "lavender")
+    .style("background-color", "whitesmoke")
 ;
 
 function getColor (datapoint,i) {
