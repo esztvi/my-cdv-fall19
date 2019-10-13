43c35d4a8c13c9598521a2759ce78adf885aa8d4
Eszter <ev821@nyu.edu>
2019-10-13 12:03:34 +0800

    UGH

--- my-work/data-zine/middlescript.js
+++ my-work/data-zine/middlescript.js
@@ -1,4 +1,4 @@
-let w = 1200;
+let w = 2400;
 let h = 800;
 
 let viz = d3.select("#container")
@@ -163,28 +163,17 @@ function gotData(incomingData){
 
 
 
-  let mdToDateObjectConverter = d3.utcParse("%Y-%m-%dT%H:%M:%S.%LZ");
+    let mdToDateObjectConverter = d3.utcParse("%Y-%m-%dT%H:%M:%S.%LZ");
   var formatMonth = d3.timeFormat("%m"),
-    formatDay = d3.timeFormat("%dT"),
-    formatTime = d3.timeFormat("%H:%M:%S"),
-    date = new Date(d.time); // Thu May 01 2014 00:00:00 GMT-0700 (PDT)
-
-  // next we can create an x scale. we want the x axis to sretch all the way from
-  // the first year (min) of HIV measurement in our data set to the most recent (max) one.
-  // then we want this information in this format:
-  // [first-year-of-measurement, most-recent-year-of-measurement]
-  // in order to define it as our x Scales input Domain.
-  // d3 gives us useful methods to do this:
-  // We can get the minimum/maximum value of any key in any array of objects or values. It doesn't
-  // matter how complex out data is structured because we can tell d3 precisely
-  // where to look for the value of which we want to know the minimum/maximum.
-  // take a look:
-  let xDomain = d3.extent(incomingData, function(d){
+    formatDay = d3.timeFormat("%d"),
+    formatTime = d3.timeFormat("%H:%M"),
+    time = new Date(formatTime); // Thu May 01 2014 00:00:00 GMT-0700 (PDT)
+  let xDomain = d3.extent(time, function(d){
     // let day = d.time(mdToDateObjectConverter);
     console.log(d);
-    console.log(mdToDateObjectConverter(d.time));
-    let properlyFormatted = mdToDateObjectConverter(d.time)
-    // console.log(properlyFormatted);
+    console.log(mdToDateObjectConverter(time));
+    let properlyFormatted = mdToDateObjectConverter(time)
+    console.log(properlyFormatted);
     //IMPORTANT: whatever we return will be the value of which d3 will
     // look for the minimum. Ultimately it returns the ONE minimum value of all data points.
     return properlyFormatted;
@@ -192,58 +181,23 @@ function gotData(incomingData){
 
 
   console.log(xDomain);
-  // Let's see what we got:
-  // console.log(minDay);
-  // // this returns this Date object:
-  // // Mon Jan 01 1990 00:00:00 GMT+0800 (China Standard Time)
-  // // 1990! looks good!
-  // // the maximum
-  // let maxDay = d3.max(mdToDateObjectConverter, function(d){
-  //   return mdToDateObjectConverter(day);
-  // });
-  // // the only thing that changed is the word "max"
-  // console.log(maxDay);
-  // // Sun Jan 01 2017 00:00:00 GMT+0800 (China Standard Time)
-  // // 2017! Great, now we can put them into a domain array that we supply to constructing the xScale
-  // let xDomain = [minDay, maxDay];
-  // this will work perfectly well. But because it is needed so often
-  // and requires the same code with only one word changed (min->max),
-  // there is an even better method that directly returns min AND max in an array:
-  // let alternativeXDomain = d3.extent(properlyFormattedDate, function(d){
-  //   return mdToDateObjectConverter(d.time);
-  // })
-  // check it out
-  // console.log(alternativeXDomain);
-  // [Mon Jan 01 1990 00:00:00 GMT+0800 (China Standard Time), Sun Jan 01 2017 00:00:00 GMT+0800 (China Standard Time)]
-  // You see? the exact same, just much shorter!
-  // so right now the variables xDomain and alternativeXDomain have the exact same value.
-  // the only difference is that we create alternativeXDomain much smarter.
-  // X scale (as learned in week4's Lab)
 
 
-formatMonth(date); // "May"
-formatDay(date); // "Thursday"
-  let xPadding = 50;
+// formatDay(date);
+formatTime(time);
+  let xPadding = 30;
   // reference: https://github.com/d3/d3-scale#time-scales
-  let xScale = d3.scaleTime().domain(XDomain).range([xPadding, w-(xPadding*2)]);
-  // console.log(xScale(xDomain("9/10")))
-  // Next, let's draw an x axis. This is new for us, but luckily D3 makes it incredibly
-  // easy.
-  // reference: https://github.com/d3/d3-axis
-  // All an axis realy needs is to know our scale
+  let xScale = d3.scaleTime().domain(xDomain).range([xPadding, w-(xPadding*2)]);
+
   var xAxis = d3.axisBottom(xScale);
-  // d3 works with the default svg shapes, an axis is put together of many of them.
-  // it makes A LOT of sense to group all these elements in a group
+
   let xAxisGroup = viz.append("g").attr("class", "xaxis");
-  // then we tell D3 to construct an axis in this group:
+
   xAxisGroup.call(xAxis);
-  // take a look, do you see the axis at the top of your svg?
-  // looks good no? The "Bottom" in "d3.axisBottom(xScale);" refers to the side on which
-  // the axis text appear, not where the axis as a whole is located,
-  // we have to take care of that ourselves:
+
   let xAxisYPos = h - 30;
   xAxisGroup.attr("transform", "translate(0,"+xAxisYPos+")");
-  // excuse me please, but this. looks. mesmerizing.
+
 
 
   // y scale and axis
@@ -251,47 +205,47 @@ formatDay(date); // "Thursday"
   // "Incidence - HIV/AIDS - Sex: Both - Age: All Ages (Number) (new cases of HIV)"
   // -key. What an annoyingly long key. If you are confused look at the datapoints
   // we console.logged above. Let's save the key in a variable to make it less annoying:
-  let valueKey = "Messages By Platform";
-
-  // Now let's do it, but faster than above!
-  // y Scale:
-  let topPadding = 30;
-  let yScale = d3.scaleLinear().domain(d3.extent(properlyFormatted, function(d){return d[valueKey]})).range([xAxisYPos, topPadding]);
-  // Wow, please consider this one carfully. We get the min max extent right in place.
-  // we access the values NOT WITH A DOT NOTATATION like d.Year!!!!!! This is confusing, but
-  // extremely IMPORTANT to know. We use our string (variable) in []-brackets instead.
-  // then, our range: we want the lowest value to be scaled to the pixel where our X AXIS starts
-  // so we use the xAxisYPos for the min of the range, and a little padding for the top.
-
-  // next, axis:
-  var yAxis = d3.axisLeft(yScale);
+  let valueKey = new Date(formatDay);
+  //
+  // // Now let's do it, but faster than above!
+  // // y Scale:
+let topPadding = 30;
+let yScale = d3.scaleLinear().domain(d3.extent(incomingData, function(d){return d[valueKey]})).range([xAxisYPos, topPadding]);
+  // // Wow, please consider this one carfully. We get the min max extent right in place.
+  // // we access the values NOT WITH A DOT NOTATATION like d.Year!!!!!! This is confusing, but
+  // // extremely IMPORTANT to know. We use our string (variable) in []-brackets instead.
+  // // then, our range: we want the lowest value to be scaled to the pixel where our X AXIS starts
+  // // so we use the xAxisYPos for the min of the range, and a little padding for the top.
+  //
+  // // next, axis:
+   var yAxis = d3.axisLeft(yScale);
   let yAxisgroup = viz.append("g").attr("class", "yaxis").call(yAxis);
-  yAxisgroup.attr("transform", "translate("+xPadding+",0)");
+   yAxisgroup.attr("transform", "translate("+xPadding+",0)");
 
   // now that was quick. six lines of code?? Oh my, this is so neat.
 
 
   // now let's plot
   // to keep things seperated let's make a group for all shapes:
-  let vizGroup = viz.append("g").attr("class", "vizgroup");
-
-  // bind data and create groups for each datapoint:
-  let dataGroups = vizGroup.selectAll(".datagroup").data(filteredData).enter()
-      .append("g")
-      .attr("class", "datagroup")
-  ;
-
-
-  // OPTION 1 circles
-
-  // append circles to the groups
-
-  let circles = dataGroups.append("circle")
-      .attr("cx", 0)
-      .attr("cy", 0)
-      .attr("r", 5)
-  ;
-
+  // let vizGroup = viz.append("g").attr("class", "vizgroup");
+  //
+  // // bind data and create groups for each datapoint:
+  // let dataGroups = vizGroup.selectAll(".datagroup").data(filteredData).enter()
+  //     .append("g")
+  //     .attr("class", "datagroup")
+  // ;
+  //
+  //
+  // // OPTION 1 circles
+  //
+  // // append circles to the groups
+  //
+  // let circles = dataGroups.append("circle")
+  //     .attr("cx", 0)
+  //     .attr("cy", 0)
+  //     .attr("r", 5)
+  // ;
+  //
 
 
   // OPTION 2 graphics
@@ -304,16 +258,16 @@ formatDay(date); // "Thursday"
 
 
   // translate function in which we are using our scales
-  // NOTE: the xScale expect us to supply a properly formatted date object
-  function getTranslate(d, i){
-    let properlyFormattedDate = yearToDateObjectConverter(d.Year);
-    let value = d[valueKey];
-    return "translate("+xScale(properlyFormattedDate)+","+yScale(value)+")";
-  }
-  // translate the position of each group:
-  dataGroups.attr("transform", getTranslate);
-  // this looks great, compare it with this graph from the website we got our data from:
-  // https://ourworldindata.org/grapher/new-cases-of-hiv-infection?tab=chart&time=1990..2017&country=CHN
+  // // NOTE: the xScale expect us to supply a properly formatted date object
+  // function getTranslate(d, i){
+  //   let properlyFormattedDate = yearToDateObjectConverter(d.Year);
+  //   let value = d[valueKey];
+  //   return "translate("+xScale(properlyFormattedDate)+","+yScale(value)+")";
+  // }
+  // // translate the position of each group:
+  // dataGroups.attr("transform", getTranslate);
+  // // this looks great, compare it with this graph from the website we got our data from:
+  // // https://ourworldindata.org/grapher/new-cases-of-hiv-infection?tab=chart&time=1990..2017&country=CHN
 
 
   // what next?
