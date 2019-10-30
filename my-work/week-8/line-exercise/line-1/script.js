f52bb65904548b2c8315240c9b19a83fe0e2dc3e
Eszter <ev821@nyu.edu>
2019-10-30 14:14:25 +0800

    complete

--- my-work/week-8/line-exercise/line-1/script.js
+++ my-work/week-8/line-exercise/line-1/script.js
@@ -18,26 +18,51 @@ let data = [
   }
 ];
 
- function getX(d){
-   return d.laughs *50;
- }
+function getX(d){
+  return d.laughs *100;
+}
 
- function getY(d){
-return d.mood *50;
- }
+function getY(d){
+  return d.mood *50;
+}
 
 let w = 900;
 let h = 500;
 let circleData;
 let viz = d3.select("#container")
-  .append("svg")
-    .style("width", w)
-    .style("height", h)
-    .style("outline", "solid black")
+.append("svg")
+.style("width", w)
+.style("height", h)
+.style("outline", "solid black")
 ;
 
-viz.selectAll("viz").data(data).enter().append("circle")
-                                          .attr("cx", getX)
-                                          .attr("cy", getY)
-                                          .attr("r", 20)
+// viz.selectAll("viz").data(data).enter().append("circle")
+//                                           .attr("cx", getX)
+//                                           .attr("cy", getY)
+//                                           .attr("r", 20)
+// ;
+
+// bind data
+// let theSitu=viz.selectAll("circle").data(data);
+// let entering = theSitu.enter();
+// entering.append("circle")
+// .attr("cx",getX)
+// .attr("cy",getY)
+// .attr("r",20)
+// ;
+let lineMaker = d3.line()
+.x(getX)
+.y(getY)
 ;
+let test= lineMaker(data);
+console.log(test);
+
+let theSituation = viz.datum(data)
+theSituation.append("path")
+.attr("d",lineMaker)
+.attr("fill", "none")
+.attr("stroke", "seagreen")
+;
+
+
+// viz.append("path").attr("d",test).attr("fill", "none").attr("stroke", "seagreen");
