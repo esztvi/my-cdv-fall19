98e4a75331a9ada6057c56c2b0c07b5b69b6e10b
Eszter <ev821@nyu.edu>
2019-11-17 20:16:53 +0800

    ...

---
+++ my-work/week-11/prototype/back.js
@@ -0,0 +1,8 @@
+let w = 1200;
+let h = 800;
+let color= (2,139,211);
+let viz = d3.select("#container").append("svg")
+    .style("width", w)
+    .style("height", h)
+    .style("background-color", color)
+;
