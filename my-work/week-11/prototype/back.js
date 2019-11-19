bf58ff9c03a875b3cf34f6bfbf531846babe7355
Eszter <ev821@nyu.edu>
2019-11-19 11:20:22 +0800

    work

--- my-work/week-11/prototype/back.js
+++ my-work/week-11/prototype/back.js
@@ -4,22 +4,22 @@ let h = 800;
 let viz = d3.select("#container").append("svg")
     .style("width", w)
     .style("height", h)
-    .style("background-color","rgb(2,139,211)")
+    .style("background-color","(2,139,211)")
     // .style("background-color", #028bd3)
 ;
-function getGroupTranslation(d, i){
-  return "translate(" + w / 2 + "," + ( h/2)+ ")"; // Add 100 on Y translation, cause upper bars are longer
-
-}
+// function getGroupTranslation(d, i){
+//   return "translate(" + w / 2 + "," + ( h/2)+ ")"; // Add 100 on Y translation, cause upper bars are longer
+//
+// }
 
 function gotData(incomingData){
   console.log(incomingData);
-  let xScale = d3.scaleBand().domain([0, 26]).range([100, 800]);
+  let xScale = d3.scaleBand().domain(incomingData.map(function(d) {return d.area})).range([100, 1100]);
   // console.log(xScale);
-  let yScale = d3.scaleBand().domain(incomingData.map(function(d) {return d.industry})).range([100, 1100]);
+  let yScale = d3.scaleBand().domain([22205,1757618]).range([100, 800]);
   let ChartGroup = viz.append("g")
     .classed("ChartGroup",true)
-    .attr("transform",getGroupTranslation)
+    // .attr("transform",getGroupTranslation)
 
     // let towers = datagroups
       // .append("path")
