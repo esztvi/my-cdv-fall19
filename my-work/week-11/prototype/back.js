14233936fa826ae2661d83577177f39fecf17298
Eszter <ev821@nyu.edu>
2019-11-19 13:16:36 +0800

    ah

--- my-work/week-11/prototype/back.js
+++ my-work/week-11/prototype/back.js
@@ -36,29 +36,19 @@ let yMax = d3.max(incomingData, function(d){return d.population});
 yDomain = [0, yMax];
 let yScale = d3.scaleLinear().domain(yDomain).range([0, h-padding*2]);
 let graphGroup = viz.append("g").classed("graphGroup", true);
+let dataGroups = graphGroup.append("g").classed("dataGroups", true);
 // position the group along the x axis (check the inspector tool to see
 // what we are doing):
-graphGroup.attr("transform", function(d, i){
+dataGroups.append("rect").attr("width", function(){
+  return xScale.bandwidth();
+}).attr("height", function(d, i){
+  return yScale(d.population);
+}).attr("y", function(d,i){
+  return -yScale(d.population);
+}).attr("fill", "black").attr("transform", function(d, i){
   return "translate("+ xScale(d.area)+ "," + (h - padding) + ")"
+  ;
 })
 };
-dataGroups
-.append("rect")
-.attr("width", function(){
-  // the scaleBand we are using
-  // allows us to as how thick each band is:
-  return xScale.bandwidth();
-})
-.attr("height", function(d, i){
-  // the idea is that we make the bar
-  // as high as dictated by the value...
-  return yScale(d.population);
-})
-.attr("y", function(d,i){
-  // ...and then push the bar up since it
-  // is drawn from top to bottom
-  return -yScale(d.population);
-})
-.attr("fill", "black")
-;
+
 d3.json("citycomp.json").then(gotData);
