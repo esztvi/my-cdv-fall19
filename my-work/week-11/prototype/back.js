48b5941f39d4acf4146de4a9f6a845180975b495
Eszter <ev821@nyu.edu>
2019-11-20 16:34:01 +0800

    progress

--- my-work/week-11/prototype/back.js
+++ my-work/week-11/prototype/back.js
@@ -11,15 +11,15 @@ let viz = d3.select("#container").append("svg")
 ;
 // function getGroupTranslation(d, i){
 //   return "translate(" + w / 2 + "," + ( h/2)+ ")"; // Add 100 on Y translation, cause upper bars are longer
-//
+//250,26,9
 // }
 function getColor (datapoint,i) {
 
     console.log(datapoint.location);
     if(datapoint.location == "Hungary")
-    { return "rgb(250,26,9)";}
-    else if(datapoint.location == "Abroad")
     { return "rgb(9,130,71)";}
+    else if(datapoint.location == "Abroad")
+    { return "rgb(250,26,9)";}
 };
 
 function getCountry (datapoint,i) {
@@ -27,8 +27,28 @@ function getCountry (datapoint,i) {
     console.log(datapoint.country);
     if(datapoint.country == "Hungary")
     { return "rgb(250,26,9)";}
-    else if(datapoint.location == "Abroad")
-    { return "rgb(9,130,71)";}
+    else if(datapoint.country == "Romania")
+    { return "rgb(252,209,21)";}
+    else if(datapoint.country == "Ukraine")
+    { return "rgb(0, 91, 187)";}
+    else if(datapoint.country == "England")
+    { return "rgb((0, 36, 125)";}
+    else if(datapoint.country == "United States")
+    { return "rgb(60, 59, 110)";}
+    else if(datapoint.country == "Canada")
+    { return "rgb(255, 0, 0)";}
+    else if(datapoint.country == "Australia")
+    { return "rgb(0, 0, 139)";}
+    else if(datapoint.country == "Austria")
+    { return "rgb(237, 41, 57)";}
+    else if(datapoint.country == "Switzerland")
+    { return "rgb(213, 43, 30)";}
+    else if(datapoint.country == "Sweden")
+    { return "rgb(255, 205, 0)";}
+    else if(datapoint.country == "Germany")
+    { return "rgb(0, 0, 0)";}
+    else if(datapoint.country == "Russia")
+    { return "rgb(0, 51, 160)";}
 };
 function gotData(incomingData){
   console.log(incomingData);
@@ -73,6 +93,28 @@ d3.select(".dataGroups")
     .style("width", function(d) { return x(d) + "px"; })
     .text(function(d) { return d; });
 };
+function removeTransitions(){
+					d3.select("rect").transition().attr("transform", "translate(0)").style("fill", getColor);
+				}
+        function runTransitions(){
 
+        					var HUCheckboxSelected = d3.select("#HU").property("checked");
+        					var OTHERCheckboxSelected = d3.select("#OTHER").property("checked");
+        					var bothCheckboxSelected = d3.select("#both").property("checked");
+
+        					if(!HUCheckboxSelected && !OTHERCheckboxSelected && !bothCheckboxSelected){
+        						return;
+        					}
+                  		var rectTransition = d3.select("rect").transition();
+                      if(HUCheckboxSelected){
+						rectTransition.attr("transform", "translate(150)").duration(1000);
+					}
+					if(OTHERCheckboxSelected){
+						rectTransition.attr("width", "100").attr("height", "100").duration(1000);
+					}
+					if(bothCheckboxSelected){
+						rectTransition.style("fill",getCountry).duration(1000);
+					}
+        }
 
 d3.json("citycomp.json").then(gotData);
--- my-work/week-11/scrollexample/script.js
+++ my-work/week-11/scrollexample/script.js
@@ -9,18 +9,40 @@ import currentBox from "./leonScroller.js";
 // that are defined at the bottom)
 let w, h;
 let heightRatio = 1;
-let padding = 90;
+let padding = 30;
 
 let viz = d3.select("#visualization")
     .append("svg")
   .style("background-color", "lavender")
 ;
+
 // function to adjust viz height dynamically
 // in order to keep the heightRatio at any given
 // width of the browser window
 // (function definition at the bottom)
 adjustVizHeight();
+function gotData(incomingData){
+  console.log(incomingData);
+  let countryData = incomingData.map(function(d){return d.gender});
 
+  console.log(countryData);
+  let xScale = d3.scaleBand()
+  .domain(countryData)
+  .range([padding, w-padding])
+  .paddingInner(0.1)
+  ;
+  let xAxis = d3.axisBottom(xScale)
+  xAxis.tickFormat(d=>{return incomingData.filter(dd=>dd.gender==d)[0].gender;});
+  let xAxisGroup = viz.append("g").classed("xAxis", true);
+  xAxisGroup.call(xAxis);
+  xAxisGroup.attr("transform", "translate(0,"+ (h-padding) +")").style("color", "black").style("stroke-width","2");
+  let yMax = d3.max(incomingData, function(d){return d["pubdate-yr"]});
+  console.log(d.pubdate_yr);
+  let yDomain = [0, yMax];
+  let yScale = d3.scaleLinear().domain(yDomain).range([0, h-padding*2]);
+  let graphGroup = viz.append("g").classed("graphGroup", true);
+  let dataGroups = graphGroup.append("g").classed("dataGroups", true);
+}
 
 // your script starts here, e.g. load data here.
 
@@ -50,7 +72,6 @@ d3.select("#textboxes").on("scroll", function(){
 
 
 
-
 // function to adjust viz height dynamically
 // in order to keep the heightRatio at any given
 // width of the browser window
@@ -65,3 +86,5 @@ function resized(){
   adjustVizHeight()
 }
 window.addEventListener("resize", resized);
+
+d3.tsv("translatedbooks.tsv").then(gotData);
