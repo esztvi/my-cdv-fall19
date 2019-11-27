2c3a9156f18f9e4cd3cbdc22dbb3cf54df39ae68
Eszter <ev821@nyu.edu>
2019-11-27 16:29:53 +0800

    timeline!!!!!

--- my-work/data_story/3_decline_en/3.js
+++ my-work/data_story/3_decline_en/3.js
@@ -1,67 +1,135 @@
-import currentBox from "./scroller.js";
-// imports just one function from a different file
-// more info, import: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
-// more info, export: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
-
-// we don't hardcode w and h this time
-// but keep them responsive
-// (see adjustVizHeight and resized function
-// that are defined at the bottom)
-let w, h;
-let heightRatio = 1;
-let padding = 150;
-
-let viz = d3.select("#visualization")
-    .append("svg")
-  .style("background-color", "whitesmoke")
-;
-// function to adjust viz height dynamically
-// in order to keep the heightRatio at any given
-// width of the browser window
-// (function definition at the bottom)
-adjustVizHeight();
 
 
-// your script starts here, e.g. load data here.
-
-
-
-// scrolling event listener
-// you might move this block into the part of your code
-// in which your data is loaded/available
-let previousSection;
-d3.select("#textboxes").on("scroll", function(){
-  // the currentBox function is imported on the
-  // very fist line of this script
-  currentBox(function(box){
-    console.log(box.id);
-
-    if(box.id=="two" && box.id!=previousSection){
-      console.log("changing viz");
-      // trigger a new transition
-      previousSection = box.id;
-    }
-
+var margin = { top: 20, right: 80, bottom: 30, left: 50 },
+  width =
+    parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
+  height =
+    parseInt(d3.select("#chart").style("height")) - margin.top - margin.bottom;
+// Define date parser
+var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
+// Define scales
+var xScale = d3.scaleTime().range([0, width]);
+var yScale = d3.scaleLinear().range([height, 0]);
+var color = d3.scaleOrdinal().range(d3.schemeCategory10);
+// Define axes
+var xAxis = d3.axisBottom().scale(xScale);
+var yAxis = d3.axisLeft().scale(yScale);
+// Define lines
+var line = d3
+  .line()
+  .curve(d3.curveMonotoneX)
+  .x(function(d) {
+    return xScale(d.year);
   })
-})
-
-
-
-
-
-
-
-// function to adjust viz height dynamically
-// in order to keep the heightRatio at any given
-// width of the browser window
-function adjustVizHeight(){
-  viz.style("height", function(){
-    w = parseInt(viz.style("width"), 10);
-    h = w*heightRatio;
-    return h;
-  })
+  .y(function(d) {
+    return yScale(d.value);
+  });
+// Define svg canvas
+var svg = d3
+  .select("#chart")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// Read in data
+d3.csv("giniLine.csv").then(function(data) {
+  // Set the color domain equal to the three product categories
+  var productCategories = d3.keys(data[0]).filter(function(key) {
+    return key !== "year" && key !== "metric";
+  });
+  color.domain(productCategories);
+  // console.log(JSON.stringify(data, null, 2)) // to view the structure
+  // Format the data field
+  data.forEach(function(d) {
+    d["year"] = parseDate(d["year"]);
+  });
+  // Filter the data to only include a single metric
+  var subset = data.filter(function(el) {
+    return el.metric === "Quantity";
+  });
+  // console.log(JSON.stringify(subset, null, 2))
+  // Reformat data to make it more copasetic for d3
+  // data = An array of objects
+  // concentrations = An array of three objects, each of which contains an array of objects
+  var concentrations = productCategories.map(function(category) {
+    return {
+      category: category,
+      datapoints: subset.map(function(d) {
+        return { date: d["Order Month"], concentration: +d[category] };
+      })
+    };
+  });
+  // console.log(JSON.stringify(concentrations, null, 2)) // to view the structure
+  // Set the domain of the axes
+  xScale.domain(
+    d3.extent(subset, function(d) {
+      return d["Order Month"];
+    })
+  );
+  yScale.domain([0.25, 0.5]);
+  // Place the axes on the chart
+  svg
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+  svg
+    .append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("class", "label")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .attr("dx", ".71em")
+    .style("text-anchor", "beginning")
+    .text("Product Concentration");
+  var products = svg
+    .selectAll(".category")
+    .data(concentrations)
+    .enter()
+    .append("g")
+    .attr("class", "category");
+  products
+    .append("path")
+    .attr("class", "line")
+    .attr("d", function(d) {
+      return line(d.datapoints);
+    })
+    .style("stroke", function(d) {
+      return color(d.category);
+    });
+  // console.log(JSON.stringify(d3.values(concentrations), null, 2)) // to view the structure
+  console.log(d3.values(concentrations)); // to view the structure
+  console.log(concentrations);
+  // console.log(concentrations.map(function()))
+});
+// Define responsive behavior
+function resize() {
+  var width =
+      parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
+    height =
+      parseInt(d3.select("#chart").style("height")) -
+      margin.top -
+      margin.bottom;
+  // Update the range of the scale with new width/height
+  xScale.range([0, width]);
+  yScale.range([height, 0]);
+  // Update the axis and text with the new scale
+  svg
+    .select(".x.axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+  svg.select(".y.axis").call(yAxis);
+  // Force D3 to recalculate and update the line
+  svg.selectAll(".line").attr("d", function(d) {
+    return line(d.datapoints);
+  });
+  // Update the tick marks
+  xAxis.ticks(Math.max(width / 75, 2));
+  yAxis.ticks(Math.max(height / 50, 2));
 }
-function resized(){
-  adjustVizHeight()
-}
-window.addEventListener("resize", resized);
+// Call the resize function whenever a resize event occurs
+d3.select(window).on("resize", resize);
+// Call the resize function
+resize();
--- my-work/data_story/4_timeline_en/4.js
+++ my-work/data_story/4_timeline_en/4.js
@@ -1,11 +1,86 @@
 let w = 1425;
-let h = 800;
+let h = 700;
 let padding = 50;
 // let side_padding = ;
 // let color= (2,139,211);
+function getSize(datapoint,i) {
+
+    console.log(datapoint.size);
+    if(datapoint.size == 1)
+    { return 5;}
+    else if(datapoint.size == 2)
+    { return 10;}
+    else if(datapoint.size == 3)
+    { return 20;}
+
+    return "datapoint.medium";
+};
 let viz = d3.select("#container").append("svg")
     .style("width", w)
     .style("height", h)
-    .style("border","black")
-  .style("background-color", "black")
+
+  // .style("background-color", "lavender")
 ;
+let xScaleCountry= d3.scaleBand().range([padding,w-padding]);
+let XScalePosNeg= d3.scaleBand().range([padding,w-padding]);
+let xScaleYear= d3.scaleTime().range([padding,w-padding]);
+d3.json("data.json").then(function(incomingData){
+  console.log(incomingData);
+
+  //modify scale
+  let timeParse= d3.timeParse("%Y");
+  console.log(timeParse(2015));
+
+  let extent=d3.extent(incomingData,function(d){
+    return timeParse(d.year);
+  })
+  console.log(extent);
+  xScaleYear.domain(extent);
+  let xAxisGroup=viz.append("g").attr("class","xaxisgroup").attr("fill","whitesmoke").attr("stroke","whitesmoke").attr("line","whitesmoke");
+  let xAxis=d3.axisBottom(xScaleYear);
+  let xAxisYPos = h -50;
+  xAxisGroup.attr("transform", "translate(0,"+xAxisYPos+")");
+  xAxisGroup.call(xAxis);
+  // let domain=[minvalue,maxvalue];
+  incomingData.forEach(d=>{
+    d.x= xScaleYear(timeParse(d.year));
+    d.y=h/2
+  })
+  // viz.selectAll(".datapoint").data(incomingData).enter().append("circle").attr("class", "datapoint").attr("cx",function(d){return xScaleYear(timeParse(d.year))}).attr("cy",h/2).attr("r",4);
+
+  let simulatiion= d3.forceSimulation(incomingData)
+  // .force("forceX",d3.forceX(function(d){
+  //   return xScaleYear(timeParse(d.year))
+  // }))
+  // .force("forceY",d3.forceY(h/2))
+  .force("collide",d3.forceCollide(20))
+  // .on("tick", simulationRan)
+  .on("end",simulationEnded)
+    .tick(100)
+  ;
+  function simulationEnded(){
+    // console.log("just ran");
+    // console.log(incomingData[0].x);
+    viz.selectAll(".datapoint").data(incomingData).enter().append("circle").transition().attr("cx",function(d){
+      return d.x;
+    })
+    .attr("cy",function(d){
+      return d.y;
+    }).attr("r",getSize).attr("fill","whitesmoke");
+
+  }
+
+
+
+  // console.log(incomingData);
+  // function simulationRan(){
+  //   // console.log("just ran");
+  //   console.log(incomingData[0].x);
+  //   viz.selectAll(".datapoint").transition().attr("cx",function(d){
+  //     return d.x;
+  //   })
+  //   .attr("cy",function(d){
+  //     return d.y;
+  //   })
+  // }
+})
