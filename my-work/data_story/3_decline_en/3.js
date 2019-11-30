3f612db49c48f8484d172b696cb49ae12a91de74
Eszter <ev821@nyu.edu>
2019-11-30 13:34:11 +0800

    3-line graph

--- my-work/data_story/3_decline_en/3.js
+++ my-work/data_story/3_decline_en/3.js
@@ -1,135 +1,98 @@
+// set the dimensions and margins of the graph
+let margin = {top: 20, right: 40, bottom: 30, left: 50},
+    width = 1425 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+    // width1 = 1425 - margin.left- margin.right-12,
 
+// parse the date / time
+let parseTime = d3.timeParse("%Y");
 
-var margin = { top: 20, right: 80, bottom: 30, left: 50 },
-  width =
-    parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
-  height =
-    parseInt(d3.select("#chart").style("height")) - margin.top - margin.bottom;
-// Define date parser
-var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
-// Define scales
-var xScale = d3.scaleTime().range([0, width]);
-var yScale = d3.scaleLinear().range([height, 0]);
-var color = d3.scaleOrdinal().range(d3.schemeCategory10);
-// Define axes
-var xAxis = d3.axisBottom().scale(xScale);
-var yAxis = d3.axisLeft().scale(yScale);
-// Define lines
-var line = d3
-  .line()
-  .curve(d3.curveMonotoneX)
-  .x(function(d) {
-    return xScale(d.year);
-  })
-  .y(function(d) {
-    return yScale(d.value);
-  });
-// Define svg canvas
-var svg = d3
-  .select("#chart")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+// set the ranges
+let x = d3.scaleTime().range([0, width]);
+let y0 = d3.scaleLinear().range([height, 0]);
+let y1 = d3.scaleLinear().range([height, 0]);
+let y2 = d3.scaleLinear().range([height, 0]);
+
+// define the 1st line
+let valueline = d3.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y0(d.population); });
+
+// define the 2nd line
+let valueline2 = d3.line()
+    .x(function(d) { return x(d.year); })
+    .y(function(d) { return y1(d.fertilityRate); });
+
+    // define the 3rd line
+    let valueline3 = d3.line()
+        .x(function(d) { return x(d.year); })
+        .y(function(d) { return y2(d.medianAge); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+
+let svg = d3.select("#container").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-// Read in data
-d3.csv("giniLine.csv").then(function(data) {
-  // Set the color domain equal to the three product categories
-  var productCategories = d3.keys(data[0]).filter(function(key) {
-    return key !== "year" && key !== "metric";
-  });
-  color.domain(productCategories);
-  // console.log(JSON.stringify(data, null, 2)) // to view the structure
-  // Format the data field
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.json("data.json", function(data) {
+console.log(data);
+  // format the data
   data.forEach(function(d) {
-    d["year"] = parseDate(d["year"]);
+      d.year = parseTime(d.year);
+      d.population = +d.population;
+      d.fertilityRate = +d.fertilityRate;
+      d.medianAge = +d.medianAge;
   });
-  // Filter the data to only include a single metric
-  var subset = data.filter(function(el) {
-    return el.metric === "Quantity";
-  });
-  // console.log(JSON.stringify(subset, null, 2))
-  // Reformat data to make it more copasetic for d3
-  // data = An array of objects
-  // concentrations = An array of three objects, each of which contains an array of objects
-  var concentrations = productCategories.map(function(category) {
-    return {
-      category: category,
-      datapoints: subset.map(function(d) {
-        return { date: d["Order Month"], concentration: +d[category] };
-      })
-    };
-  });
-  // console.log(JSON.stringify(concentrations, null, 2)) // to view the structure
-  // Set the domain of the axes
-  xScale.domain(
-    d3.extent(subset, function(d) {
-      return d["Order Month"];
-    })
-  );
-  yScale.domain([0.25, 0.5]);
-  // Place the axes on the chart
-  svg
-    .append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-  svg
-    .append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("class", "label")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .attr("dx", ".71em")
-    .style("text-anchor", "beginning")
-    .text("Product Concentration");
-  var products = svg
-    .selectAll(".category")
-    .data(concentrations)
-    .enter()
-    .append("g")
-    .attr("class", "category");
-  products
-    .append("path")
-    .attr("class", "line")
-    .attr("d", function(d) {
-      return line(d.datapoints);
-    })
-    .style("stroke", function(d) {
-      return color(d.category);
-    });
-  // console.log(JSON.stringify(d3.values(concentrations), null, 2)) // to view the structure
-  console.log(d3.values(concentrations)); // to view the structure
-  console.log(concentrations);
-  // console.log(concentrations.map(function()))
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.year; }));
+  y0.domain([0, d3.max(data, function(d) {return Math.max(d.population);})]);
+  y1.domain([0, d3.max(data, function(d) {return Math.max(d.fertilityRate); })]);
+  y2.domain([0, d3.max(data, function(d) {return Math.max(d.medianAge); })]);
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // Add the valueline2 path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .style("stroke", "red")
+      .attr("d", valueline2);
+
+      // Add the valueline3 path.
+      svg.append("path")
+          .data([data])
+          .attr("class", "line")
+          .style("stroke", "green")
+          .attr("d", valueline3);
+
+  // Add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Add the Y0 Axis
+  svg.append("g")
+      .attr("class", "axisSteelBlue")
+      .call(d3.axisLeft(y0));
+
+  // Add the Y1 Axis
+  svg.append("g")
+      .attr("class", "axisRed")
+      .attr("transform", "translate( " + width + ", 0 )")
+      .call(d3.axisRight(y1));
+      // Add the Y2 Axis
+      svg.append("g")
+          .attr("class", "axisGreen")
+          .attr("transform", "translate( " + width + ", 0 )")
+          .call(d3.axisRight(y2));
 });
-// Define responsive behavior
-function resize() {
-  var width =
-      parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
-    height =
-      parseInt(d3.select("#chart").style("height")) -
-      margin.top -
-      margin.bottom;
-  // Update the range of the scale with new width/height
-  xScale.range([0, width]);
-  yScale.range([height, 0]);
-  // Update the axis and text with the new scale
-  svg
-    .select(".x.axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-  svg.select(".y.axis").call(yAxis);
-  // Force D3 to recalculate and update the line
-  svg.selectAll(".line").attr("d", function(d) {
-    return line(d.datapoints);
-  });
-  // Update the tick marks
-  xAxis.ticks(Math.max(width / 75, 2));
-  yAxis.ticks(Math.max(height / 50, 2));
-}
-// Call the resize function whenever a resize event occurs
-d3.select(window).on("resize", resize);
-// Call the resize function
-resize();
