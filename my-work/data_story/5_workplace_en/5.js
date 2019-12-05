4f6fdabcbfa07647f3f5b275d4f61f312dd8275c
Eszter <ev821@nyu.edu>
2019-12-05 21:47:27 +0800

    changes

--- my-work/data_story/5_workplace_en/5.js
+++ my-work/data_story/5_workplace_en/5.js
@@ -1,13 +1,316 @@
 let w = 1425;
-let h = 800;
+let h = 2700;
 let padding = 50;
 
 
 let viz = d3.select("#container").append("svg")
-    .style("width", w)
-    .style("height", h)
-  .style("background-color", "lavender")
+.style("width", w)
+.style("height", h)
+// .style("background-color", "lavender")
 ;
 d3.json("data.json").then(function(incomingData){
   console.log(incomingData);
+  let xScaleAg= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleFor= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleFI= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleMin= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleProc= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleEn= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleBuild= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleTrade= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleHR= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleTransp= d3.scaleLinear().range([padding,w-padding]);
+  let xScalePC= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleFI= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleRE= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleFor= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleFIR= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleRE= d3.scaleLinear().range([padding,w-padding]);
+  let xScalePA= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleEd= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleHSA= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleServ= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleSAA= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleOrg= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleUn= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleGS= d3.scaleLinear().range([padding,w-padding]);
+  let xScalePaint= d3.scaleLinear().range([padding,w-padding]);
+  let xScalePCB= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleApar= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleFS= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleRoof= d3.scaleLinear().range([padding,w-padding]);
+  let xScaleWFE= d3.scaleLinear().range([padding,w-padding]);
+  let boxAg= viz.append("rect")
+  .attr("class","boxAg")
+  .attr("x", 50)
+  .attr("y", 0)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rgb(0)")
+  // .on(
+  //   "click", revealAg
+  // );
+  let boxFor= viz.append("rect")
+  .attr("class","boxFor")
+  .attr("x", 50)
+  .attr("y", 100)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rgb(255,255,0)")
+  // .on(
+  //   "click", revealFor
+  // );
+
+  let boxFI= viz.append("rect")
+  .attr("class","boxFi")
+  .attr("x", 50)
+  .attr("y", 200)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rgb(0,255,255)")
+  // .on(
+  //   "click", revealFi
+  // );
+  let boxMin= viz.append("rect")
+  .attr("class","boxMin")
+  .attr("x", 50)
+  .attr("y", 300)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rgb(255,0,255)")
+  // .on(
+  //   "click", revealMin
+  // );
+  let boxProc= viz.append("rect")
+  .attr("class","boxProc")
+  .attr("x", 50)
+  .attr("y", 400)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rgb(255,0,0)")
+  // .on(
+  //   "click", revealProc
+  // );
+  let boxEn= viz.append("rect")
+  .attr("class","boxEn")
+  .attr("x", 50)
+  .attr("y", 500)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rgb(0,255,0)")
+  // .on(
+  //   "click", revealEn
+  // );
+  let boxBuild= viz.append("rect")
+  .attr("class","boxBuild")
+  .attr("x", 50)
+  .attr("y", 600)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rgb(0,0,255)")
+  // .on(
+  //   "click", revealBuild
+  // );
+  let boxTrade= viz.append("rect")
+  .attr("class","boxTr")
+  .attr("x", 50)
+  .attr("y", 700)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","lavender")
+  // .on(
+  //   "click", revealTrade
+  // );
+  let boxHR= viz.append("rect")
+  .attr("class","boxHR")
+  .attr("x", 50)
+  .attr("y", 800)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","tomato")
+  // .on(
+  //   "click", revealHR
+  // );
+  let boxTransp= viz.append("rect")
+  .attr("class","boxTransp")
+  .attr("x", 50)
+  .attr("y", 900)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","gold")
+  // .on(
+  //   "click", revealTransp
+  // );
+  let boxPC= viz.append("rect")
+  .attr("class","boxPC")
+  .attr("x", 50)
+  .attr("y", 1000)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","maroon")
+  // .on(
+  //   "click", revealPC
+  // );
+  let boxFIR= viz.append("rect")
+  .attr("class","boxFIR")
+  .attr("x", 50)
+  .attr("y", 1100)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","midnightblue")
+  // .on(
+  //   "click", revealFI
+  // );
+  let boxRE= viz.append("rect")
+  .attr("class","boxRE")
+  .attr("x", 50)
+  .attr("y", 1200)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","teal")
+  // .on(
+  //   "click", revealRE
+  // );
+  let boxPA= viz.append("rect")
+  .attr("class","boxPA")
+  .attr("x", 50)
+  .attr("y", 1300)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","coral")
+  // .on(
+  //   "click", revealPA
+  // );
+  let boxEd= viz.append("rect")
+  .attr("class","boxEd")
+  .attr("x", 50)
+  .attr("y", 1400)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","blueviolet")
+  // .on(
+  //   "click", revealEd
+  // );
+  let boxHSA= viz.append("rect")
+  .attr("class","boxHSA")
+  .attr("x", 50)
+  .attr("y", 1500)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","green")
+  // .on(
+  //   "click", revealHSA
+  // );
+  let boxServ= viz.append("rect")
+  .attr("class","boxServ")
+  .attr("x", 50)
+  .attr("y", 1600)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","lawngreen")
+  // .on(
+  //   "click", revealServ
+  // );
+  let boxSAA= viz.append("rect")
+  .attr("class","boxSAA")
+  .attr("x", 50)
+  .attr("y", 1700)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","powderblue")
+  // .on(
+  //   "click", revealSAA
+  // );
+  let boxOrg= viz.append("rect")
+  .attr("class","boxOrg")
+  .attr("x", 50)
+  .attr("y", 1800)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","gray")
+  // .on(
+  //   "click", revealOrg
+  // );
+  let boxUn= viz.append("rect")
+  .attr("class","boxUn")
+  .attr("x", 50)
+  .attr("y", 1900)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","white")
+  // .on(
+  //   "click", revealUn
+  // );
+  let boxGS= viz.append("rect")
+  .attr("class","boxGS")
+  .attr("x", 50)
+  .attr("y", 2000)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","hotpink")
+  // .on(
+  //   "click", revealGS
+  // );
+  let boxPaint= viz.append("rect")
+  .attr("class","boxPaint")
+  .attr("x", 50)
+  .attr("y", 2100)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","violet")
+  // .on(
+  //   "click", revealPaint
+  // );
+  let boxPCB= viz.append("rect")
+  .attr("class","boxPCB")
+  .attr("x", 50)
+  .attr("y", 2200)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","rebeccapurple")
+  // .on(
+  //   "click", revealPCB
+  // );
+  let boxApar= viz.append("rect")
+  .attr("class","boxApar")
+  .attr("x", 50)
+  .attr("y", 2300)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","silver")
+  // .on(
+  //   "click", revealApar
+  // );
+  let boxFS= viz.append("rect")
+  .attr("class","boxFS")
+  .attr("x", 50)
+  .attr("y", 2400)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","indianred")
+  // .on(
+  //   "click", revealFS
+  // );
+  let boxRoof= viz.append("rect")
+  .attr("class","boxRoof")
+  .attr("x", 50)
+  .attr("y", 2500)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","olive")
+  // .on(
+  //   "click", revealRoof
+  // );
+  let boxWFE= viz.append("rect")
+  .attr("class","boxWFE")
+  .attr("x", 50)
+  .attr("y", 2600)
+  .attr("width", 1300)
+  .attr("height", 100)
+  .attr("fill","khaki")
+  // .on(
+  //   "click", revealWFE
+  // );
+
+
 });
--- my-work/data_story/8_populationcomp_en/8.js
+++ my-work/data_story/8_populationcomp_en/8.js
@@ -2,13 +2,6 @@ let w = 1425;
 let h = 800;
 let padding = 50;
 
-// let side_padding = ;
-// let color= (2,139,211);
-// let viz = d3.select("#container").append("svg")
-//     .style("width", w)
-//     .style("height", h)
-//     .style("border","black")
-//   .style("background-color", "black")
 let viz = d3.select("#container")
 .append("svg")
 .attr("width", w)
@@ -18,6 +11,9 @@ let viz = d3.select("#container")
 // .attr("viewBox", "0 0 " + w + " " + h)
 .classed("svg-content", true);
 ;
+
+
+
 let hungary_half = viz.append("g")
 .attr("class","hungary")
 .attr("width", w/2)
@@ -41,13 +37,24 @@ let other_half = viz.append("g")
 //   .attr("fill", "green")
 //
 // ;
+hungary_half.append("g")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 1)
+  // .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "20px")
+  .style("border-radius", "1px")
+  .style("padding", "5px")
+  .style("position","fixed")
+  ;
 
 d3.json("../8_populationcomp_en/County/hungary.geojson").then(function(CountriesData){
   d3.csv("data.csv").then(function(CitiesData){
     // d3.json("../8_populationcomp_en/comp/bavaria.geojson").then(function(bavariaData){
     // console.log(bavariaData);
     d3.json("compdataComplete.json").then(function(compData){
-      console.log(compData);
+      // console.log(compData);
 
       let projection = d3.geoMercator()
       .translate([w/2/2 , h/2 ])
@@ -79,58 +86,58 @@ d3.json("../8_populationcomp_en/County/hungary.geojson").then(function(Countries
       .on("mouseover",function(d,i){
         d3.select(this).attr("fill","grey").attr("stroke-width",2);
         // which county are we hovering
-        console.log(d.properties.correlatedData[" City"] + " -> " + d.properties.correlatedData[" Correlating Area"], d.properties.correlatedData);
-        if(d.properties.correlatedData[" Correlating Area"] == " Bavaria"){
+        console.log(d.properties.correlatedData["City"] + " -> " + d.properties.correlatedData["Correlating Area"], d.properties.correlatedData);
+        if(d.properties.correlatedData["Correlating Area"] == "Bavaria"){
           drawCountry("bavaria")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Ukraine"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Ukraine"){
           drawCountry("ukraine")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Ohio"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Ohio"){
           drawCountry("ohio")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Michigan"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Michigan"){
           drawCountry("michigan")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Florida"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Florida"){
           drawCountry("florida")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " New York"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "New York"){
           drawCountry("newyork")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Alberta"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Alberta"){
           drawCountry("alberta")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " New Jersey"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "New Jersey"){
           drawCountry("newjersey")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Russia"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Russia"){
           drawCountry("russia")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " British Columbia"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "British Columbia"){
           drawCountry("bc")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Austria"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Austria"){
           drawCountry("austria")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Australia"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Australia"){
           drawCountry("australia")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Switzerland"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Switzerland"){
           drawCountry("switzerland")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " London"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "London"){
           drawCountry("london")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Pennsylvania"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Pennsylvania"){
           drawCountry("pennsylvania")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Sweden"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Sweden"){
           drawCountry("sweden")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Ontario"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Ontario"){
           drawCountry("ontario")
-        }else if(d.properties.correlatedData[" Correlating Area"] == " Baden-Württemberg"){
+        }else if(d.properties.correlatedData["Correlating Area"] == "Baden-Württemberg"){
             drawCountry("baden")
-          }else if(d.properties.correlatedData[" Correlating Area"] == " Romania"){
+          }else if(d.properties.correlatedData["Correlating Area"] == "Romania"){
               drawCountry("romania")
-            }else if(d.properties.correlatedData[" Correlating Area"] == " California"){
+            }else if(d.properties.correlatedData["Correlating Area"] == "California"){
                 drawCountry("california")}
-          // return tooltip.style("hidden", false).html(d.name);
+          return tooltip.style("hidden", false).html(d.name);
         })
-        .on("mousemove",function(d){
-          // tooltip.classed("hidden", false)
-          //        .style("top", (d3.event.pageY) + "px")
-          //        .style("left", (d3.event.pageX + 10) + "px")
-          //        .html(d.name);
+        .on("mousemove",function(CitiesData){
+          tooltip.classed("hidden", false)
+                 .style("top", (d3.event.pageY+10) + "px")
+                 .style("left", (d3.event.pageX + 10) + "px")
+    .html("Hungarian City : " +d.City+ " - " + d.Population + "    Correlating Region : " +d.CorrelatingArea+ " - " + d.Population2 + " Note: Maps Not to Scale" );
         })
         .on("mouseout",function(d,i){
           d3.select(this).attr("fill","white").attr("stroke-width",1);
-          // tooltip.classed("hidden", true);
+          tooltip.classed("hidden", true);
         })
         ;
 
@@ -174,15 +181,6 @@ d3.json("../8_populationcomp_en/County/hungary.geojson").then(function(Countries
         //     // .attr("class","continent")
         //     .attr("d", path);
       });
-      function ready(CitiesData) {
-        if (error) throw error;
-        var countries1 = geojson.feature(world, world.objects.countries).features;
-        countries = countries1.filter(function(d) {
-          return names.some(function(n) {
-            if (d.name == n.name) return n.city;
-          });
-        });
-      }
 
 
     });
