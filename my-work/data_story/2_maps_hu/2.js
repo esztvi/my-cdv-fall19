04f4d4e90b91e2f2c235f454acf66544354af185
Eszter <ev821@nyu.edu>
2019-12-07 21:06:59 +0800

    additions

--- my-work/data_story/2_maps_hu/2.js
+++ my-work/data_story/2_maps_hu/2.js
@@ -1,15 +1,15 @@
-let w = 900;
-let h = 800;
-let padding = 50;
-// let side_padding = ;
-// let color= (2,139,211);
-let viz = d3.select("#container").append("svg")
-    .style("width", w)
-    .style("height", h)
-    .style("border","black")
-  .style("background-color", "black")
-;
-
-let words = d3.select("#explanation_text").append("text")
-
-;
+// let w = 900;
+// let h = 800;
+// let padding = 50;
+// // let side_padding = ;
+// // let color= (2,139,211);
+// let viz = d3.select("#container").append("svg")
+//     .style("width", w)
+//     .style("height", h)
+//     .style("border","black")
+//   .style("background-color", "black")
+// ;
+//
+// let words = d3.select("#explanation_text").append("text")
+//
+// ;
